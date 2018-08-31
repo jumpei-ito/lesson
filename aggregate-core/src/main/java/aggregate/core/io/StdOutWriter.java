28c457078c91a7bf2bc482f02b22cbce75b0b517
jumpei-ito <shark.j.i@gmail.com>
2018-09-01 00:32:35 +0900

    Refactoring for wrtiting logic

--- aggregate-core/src/main/java/aggregate/core/io/StdOutWriter.java
+++ aggregate-core/src/main/java/aggregate/core/io/StdOutWriter.java
@@ -4,26 +4,24 @@ import java.util.List;
 import aggregate.core.model.ColumnSet;
 
 /**
- * 集計結果データを標準出力に出力するクラス
+ * Class for writing aggregated data to standard output.
  */
 public class StdOutWriter extends Writer {
 
   @Override
   public void write(List<ColumnSet> columnSets) {
     System.out.println("-- Start output result.");
-    if (!writeResult(columnSets)) {
-      System.out.println("--- ColumnSet is empty.");
-    }
+    writeResult(columnSets);
     System.out.println("-- End output result.");
   }
 
-  private boolean writeResult(List<ColumnSet> columnSets) {
+  private void writeResult(List<ColumnSet> columnSets) {
     if (columnSets.size() == 0) {
-      return false;
+      System.out.println("--- ColumnSet is empty.");
+    } else {
+      System.out.println(getHeaderLine(columnSets.get(0)));
+      columnSets.forEach(System.out::println);
     }
-    System.out.println(getHeaderLine(columnSets.get(0)));
-    columnSets.forEach(System.out::println);
-    return true;
   }
 
 }
